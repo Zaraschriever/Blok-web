87dc9510b69cc92893001b132367aa8f53f1bf56
Zaraschriever <74207572+Zaraschriever@users.noreply.github.com>
2021-01-06 18:16:41 +0100

    Add files via upload

--- js/dopper.js
+++ js/dopper.js
@@ -1,3 +1,4 @@
+// dropdown navigatie footer
 var plus = document.querySelector("section:nth-of-type(2) article h4")
 var dropDown = document.querySelector("section:nth-of-type(2) article ul")
 
@@ -17,8 +18,7 @@ function open(){
 }
 
 
-
-
+// image slider pagina 2
 var showFoto = document.getElementById("hoofdAfbeelding")
 
 var eersteFoto = document.querySelector(".eersteFoto")
@@ -49,83 +49,3 @@ vierdeFoto.addEventListener("click", toggleFoto3)
 function toggleFoto3(){
   showFoto.src = "images/kaart.png";
 }
-
-
-
-
-var navbar = document.querySelector(".navbar")
-var ham = document.querySelector(".ham")
-
-ham.addEventListener("click", toggleHamburger)
-
-function toggleHamburger(){
-  navbar.classList.toggle("showNav")
-  ham.classList.toggle("showClose")
-}
-
-
-
-
-$(document).on('scroll', function(){
-    if ( $(window).scrollTop() > 30) {
-        $('#container-header').addClass('change-color');
-    } else {
-        $('#container-header').removeClass('change-color');
-    }
-});
-
-
-
-
-// $(".upper").click(function () {
-//     // Adding a class to the upper list item to handle the icon animation and background change
-//     $(this).toggleClass("collapsed");
-//
-//     var collapsedMenu = this.nextElementSibling;
-//
-//     // If the submenu is already collapsed, hide
-//     if (collapsedMenu.style.maxHeight) {
-//         collapsedMenu.style.maxHeight = null;
-//
-//         // Else, set its max-height to its scroll-height.
-//     } else {
-//         collapsedMenu.style.maxHeight = collapsedMenu.scrollHeight + "px";
-//     }
-// });
-
-
-
-// var hammenu = document.querySelector(".hammenu")
-// var menu = document.querySelector(".menu")
-//
-// menu.addEventListener("click", toggleHamburger)
-//
-// // toggles hamburger menu in and out when clicking on the hamburger
-// function toggleHamburger(){
-//   hammenu.classList.toggle("showNav")
-//   menu.classList.toggle("showClose")
-// }
-//
-// var menuLinks = document.querySelectorAll(".menuLink")
-//
-// menuLinks.forEach(
-//   function(menuLink) {
-//     menuLink.addEventListener("click", toggleHamburger)
-//   }
-// )
-//
-//
-//
-//
-// function slideSlider(){
-//   $("#slider-scroller").css({"left":"0%","transition":"all 0s linear"});
-//   $("#slider-scroller").css({"left": String(parseInt($("#slider-scroller").css("left")) - 500) + "px","transition":"all 5s linear"});
-//   setTimeout(function(){moveSliderItem()}, 2635);
-// }
-//
-// function moveSliderItem(){
-//   $("#slider-scroller div").first().detach().appendTo($("#slider-scroller"));
-//   slideSlider();
-// }
-//
-// slideSlider();
